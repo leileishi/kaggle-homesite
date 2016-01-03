9afbe9cefbf1da259ec04773f3d1e3daa3daf53a
Leilei Shi <shileilei@gmail.com>
2016-01-03 21:33:47 +0000

    Add SVM model and see whether it will finish traning or not

--- pipeline.sh
+++ pipeline.sh
@@ -3,6 +3,7 @@
 ./build_features.py data/train.csv data/test.csv data/train_feature.csv data/test_feature.csv
 ./train_lr_model.py data/train_feature.csv data/test_feature.csv models/train_lr.csv models/test_lr.csv --prob --cores 1
 ./train_rf_model.py data/train_feature.csv data/test_feature.csv models/train_rf.csv models/test_rf.csv --prob --cores 1
-./ensemble_models.py data/train.csv ensemble/train_en_feature.csv ensemble/test_en_feature.csv -m models/train_rf.csv models/test_rf.csv models/train_lr.csv models/test_lr.csv
-./train_rf_model.py ensemble/train_en_feature.csv ensemble/test_en_feature.csv ensemble/train_en_pred.csv ensemble/test_en_pred.csv --cores 1
+./train_svm_model.py data/train_feature.csv data/test_feature.csv models/train_svm.csv models/test_svm.csv --prob --cores 1
+./ensemble_models.py data/train.csv ensemble/train_en_feature.csv ensemble/test_en_feature.csv -m models/train_lr.csv models/test_lr.csv models/train_rf.csv models/test_rf.csv
+./train_rf_model.py ensemble/train_en_feature.csv ensemble/test_en_feature.csv ensemble/train_en_pred.csv ensemble/test_en_pred.csv --prob --cores 1
 ./prepare_submit.py data/test.csv ensemble/test_en_pred.csv ensemble/test_submit.csv
---
+++ train_svm_model.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+
+import sklearn
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+
+# A memory effcient way to load CSV data
+def iter_loadtxt(filename, delimiter=',', skiprows=0, dtype=float):
+    def iter_func():
+        with open(filename, 'r') as infile:
+            for _ in range(skiprows):
+                next(infile)
+            for line in infile:
+                line = line.rstrip().split(delimiter)
+                for item in line:
+                    yield dtype(item)
+        iter_loadtxt.rowlength = len(line)
+
+    data = np.fromiter(iter_func(), dtype=dtype)
+    data = data.reshape((-1, iter_loadtxt.rowlength))
+    return data
+
+def main():
+    model_name = 'Support Vector Machine'
+
+    parser = argparse.ArgumentParser(usage=model_name)
+
+    parser.add_argument("train_feature", help="Input file of training features and target")
+    parser.add_argument("test_feature", help="Input file of test features")
+    parser.add_argument("train_pred", help="Output file of predicted training target")
+    parser.add_argument("test_pred", help="Output file of predicted test target")
+    parser.add_argument("--prob", action='store_true', help='Predict probability of class 1')
+    parser.add_argument("--cores", type=int, default=-1, help='Number of cores to use')
+
+    args = parser.parse_args()
+
+    print(model_name)
+
+    # Read training data and test data
+    print('Read training data and test data')
+    df_train_feature_target = iter_loadtxt(args.train_feature, dtype=np.float32, delimiter=',', skiprows=1)
+    df_test_feature = iter_loadtxt(args.test_feature, dtype=np.float32, delimiter=',', skiprows=1)
+
+    train_X = df_train_feature_target[:,:-1]
+    train_y = df_train_feature_target[:,-1]
+    test_X = df_test_feature
+
+    # Normalise train data and test data
+    scaler = StandardScaler()
+    scaler.fit(train_X)
+    train_X = scaler.transform(train_X)
+    test_X = scaler.transform(test_X)
+
+    # Model specification and parameter range
+    model = SVC(probability=True)
+    parameters = [
+        {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+        {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+    ]
+
+    # Cross validation search
+    print('Cross validation search')
+    clf = GridSearchCV(model, parameters, cv=3, scoring='roc_auc', n_jobs=args.cores, pre_dispatch=args.cores)
+    clf.fit(train_X, train_y)
+
+    # Make predictions with the best model
+    print('Make predictions with the best model')
+    train_pred = clf.predict(train_X)
+    train_pred_prob = clf.predict_proba(train_X)[:,1]
+    test_pred = clf.predict(test_X)
+    test_pred_prob = clf.predict_proba(test_X)[:,1]
+
+    # Write out the prediction result
+    print('Write out the prediction result')
+    pd.Series(train_pred_prob if args.prob else train_pred , name='Prob' if args.prob else 'Pred') \
+        .to_csv(args.train_pred, index=False, header=True)
+    pd.Series(test_pred_prob  if args.prob else test_pred, name='Prob' if args.prob else 'Pred') \
+        .to_csv(args.test_pred, index=False, header=True)
+
+    # Report the result
+    print('Report the result')
+    print('Accuracy: ', accuracy_score(train_y, train_pred))
+    print('F1:       ', f1_score(train_y, train_pred))
+    print('ROC AUC:  ', roc_auc_score(train_y, train_pred_prob))
+    print('Model: ', clf)
+    print('Best Parameter: ', clf.best_params_)
+    print('Best Score: ', clf.best_score_)
+    print('Parameter Scores: ', clf.grid_scores_)
+
+if __name__ == '__main__':
+    main()
