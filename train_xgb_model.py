720f7820c3fd2807bbe78a5c8513eb6d967d0346
Leilei Shi <shileilei@gmail.com>
2016-01-05 00:29:39 +0000

    Minor fix of model name

--- train_xgb_model.py
+++ train_xgb_model.py
@@ -12,7 +12,7 @@ from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 from xgboost import XGBClassifier
 
 def main():
-    model_name = 'Radom Forest'
+    model_name = 'XGBoost'
 
     parser = argparse.ArgumentParser(usage=model_name)
 
